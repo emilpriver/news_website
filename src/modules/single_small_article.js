3b1d314955972bde13fee1d403fc6bf6c6eca46c
Emil Priver <emil@rivercode.se>
2019-04-25 15:49:53 +0200

    init

---
+++ src/modules/single_small_article.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Single_small_article extends Component {
+    
+
+    render() {    
+        
+        console.log(this.props)
+        
+        return (
+            <div className="article">
+
+            </div>
+        );
+    }
+
+}
